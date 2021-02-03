059095ebd11e977299e2e38eb8790a6b5d4fbb8e
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-03 12:21:52 -0500

    added kelvin function, project now converts between all temps

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -6,27 +6,45 @@
 //
 
 import Foundation
-
-//input
-print("Temperature Converter")
-print("=====================")
-print ("What temperature would you like to convert to?")
-print("C - Celsius")
-print("F - Fahrenheit")
-let convertTo = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F"])//gives choices, prompts user to make a selection repeatedly until they enter a valid one
-
-let startingTemp = Double.collectInput(withPrompt: "Enter the temperature to be converted: ",
-                           minimum: nil,
-                           maximum: nil)//collects the temperature to be converted from the user
-//process and output
-switch convertTo {
-case "C":
-    let convertedTemp = convertToCelsius(startingTemp: startingTemp)
-    print("\(startingTemp)° F  is equal to \(convertedTemp)° C")//calls the function and uses it to calculate and then print the output
-case "F":
-    let convertedTemp = convertToFahrenheit(startingTemp: startingTemp)
-    print("\(startingTemp)° C  is equal to \(convertedTemp)° F")//calls the function and uses it to calculate and then print the output
-default:
-    break
+while true{
+    var celsiusTemp = 0.0
+    var convertedTemp = 0.0
+    //input
+    print("Temperature Converter")
+    print("=====================")
+    print ("What temperature system would you like to convert from?")
+    print("C - Celsius")
+    print("F - Fahrenheit")
+    print("K - Kelvin")
+    print ("Q- Quit temperature converter")
+    let startingSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])
+    if startingSystem == "Q" {
+        exit (0)
+    }
+    print ("What temperature system would you like to convert to?")
+    print("C - Celsius")
+    print("F - Fahrenheit")
+    print("K - Kelvin")
+    print ("Q- Quit temperature converter")
+    let endSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])
+    if startingSystem == "Q" {
+        exit (0)
+    let startTemp = Double.collectInput(withPrompt: "What temperature would you like to convert?", minimum: nil, maximum: nil)
+    
+    switch startingSystem{
+    case "C": celsiusTemp = startTemp
+    case "F": celsiusTemp = fahrenheitToCelsius(startingTemp: startTemp)
+    case "K": celsiusTemp = kelvinToCelsius(startingTemp: startTemp)
+    default:
+        break
+    }
+    switch endSystem{
+    case "C": print("Your temperature in Celsius is \(celsiusTemp)° C")
+    case "F": convertedTemp = celsiusToFahrenheit(startingTemp: celsiusTemp)
+        print(("Your temperature in Fahrenheit is \(convertedTemp)° F"))
+    case "K": convertedTemp = celsiusToKelvin(startingTemp: celsiusTemp)
+        print(("Your temperature in Kelvin is \(convertedTemp)° K"))
+    default:
+        break
+    }
 }
-
