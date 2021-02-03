b01e50f35222ea6d766185518fb69545abcdb17e
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-03 12:28:50 -0500

    added comments explaining code

--- TemperatureConverter/main.swift
+++ TemperatureConverter/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 print("Temperature Converter")
 print("=====================")
-while true{
+while true{//repeats until forced to exit with exit (0)
     var celsiusTemp = 0.0
     var convertedTemp = 0.0
     //input
@@ -17,19 +17,22 @@ while true{
     print("F - Fahrenheit")
     print("K - Kelvin")
     print ("Q- Quit temperature converter")
-    let startingSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])
+    let startingSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])//gets starting temp system
+    
     if startingSystem == "Q" {
-        exit (0)
-    }
+        exit (0)}//if quit is entered, exit infinite loop
+    
     print ("What temperature system would you like to convert to?")
     print("C - Celsius")
     print("F - Fahrenheit")
     print("K - Kelvin")
     print ("Q- Quit temperature converter")
-    let endSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])
-    if startingSystem == "Q" {
+    let endSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])//gets temp system to be converted to
+   
+    if startingSystem == "Q" {//if quit is entered, exit infinite loop
         exit (0)}
-    let startTemp = Double.collectInput(withPrompt: "What temperature would you like to convert? ", minimum: nil, maximum: nil)
+    
+    let startTemp = Double.collectInput(withPrompt: "What temperature would you like to convert? ", minimum: nil, maximum: nil)//gets temp to be converted
     
     switch startingSystem{
     case "C": celsiusTemp = startTemp
@@ -37,7 +40,8 @@ while true{
     case "K": celsiusTemp = kelvinToCelsius(startingTemp: startTemp)
     default:
         break
-    }
+    }//converts temp to celsius
+    
     switch endSystem{
     case "C": print("Your temperature in Celsius is \(celsiusTemp)° C")
     case "F": convertedTemp = celsiusToFahrenheit(startingTemp: celsiusTemp)
@@ -46,6 +50,6 @@ while true{
         print(("Your temperature in Kelvin is \(convertedTemp)° K"))
     default:
         break
-    }
+    }//converts celsius temp to correct temp system
     print("====================")
 }
