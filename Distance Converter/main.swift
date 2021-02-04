816c8f575af3f77e060262d24e5c9f9e0cdcbfa7
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-03 19:31:21 -0500

    created converter file

---
+++ Distance Converter/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  TemperatureConverter
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-03.
+//
+
+import Foundation
--- Knitting Gauge Converter/main.swift
+++ Knitting Gauge Converter/main.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+print("Knitting Gauge Converter")
+print("========================")
+print("Please knit a gauge swatch using the same yarn and needle size you will be using for your project")
+let swatchWidth = Double.collectInput(withPrompt: "What temperature would you like to convert? ", minimum: nil, maximum: nil)//gets temp to be converted
+
+print ("How many stitches wide is your gauge?")
+print("C - Celsius")
+print("F - Fahrenheit")
+print("K - Kelvin")
+print ("Q- Quit temperature converter")
+let startingSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])//gets starting temp system
+
+print ("Which units do you use to measure distance?")
+print("C - Celsius")
+print("F - Fahrenheit")
+print("K - Kelvin")
+print ("Q- Quit temperature converter")
+let startingSystem = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["C", "F", "K", "Q"])//gets starting temp system
