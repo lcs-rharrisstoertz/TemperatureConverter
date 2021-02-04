55392088be2bf4239af91883edcb84e9f7f5d46b
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-03 19:40:18 -0500

    started adding input sections

--- Knitting Gauge Converter/main.swift
+++ Knitting Gauge Converter/main.swift
@@ -8,8 +8,24 @@
 import Foundation
 print("Knitting Gauge Converter")
 print("========================")
-print("Please knit a gauge swatch using the same yarn and needle size you will be using for your project")
-let swatchWidth = Double.collectInput(withPrompt: "What temperature would you like to convert? ", minimum: nil, maximum: nil)//gets temp to be converted
+print("Please knit a square or rectangular gauge swatch using the same yarn and needle size you will be using for your project")
+print("========================")
+let swatchStitchWidth = Double.collectInput(withPrompt: "How many stitches wide is your gauge swatch? ", minimum: 0, maximum: nil)
+let swatchStitchLength = Double.collectInput(withPrompt: "How many rows long is your gauge swatch? ", minimum: 0, maximum: nil)
+print("========================")
+print("Please measure the length and width of your gauge swatch")
+print("========================")
+print ("Which unit of distance are your measurements in?")
+print("1 - inches")
+print("2 - feet")
+print("3 - centimeters")
+print("4 - millimeters")
+let startingUnit = String.collectInput(withPrompt: "Please enter your selection: ", acceptableValues: ["1", "2", "3", "4"])
+
+let swatchLength = Double.collectInput(withPrompt: "How many units long is your gauge swatch? ", minimum: 0, maximum: nil)
+let swatchWidth = Double.collectInput(withPrompt: "How many units wide is your gauge swatch? ", minimum: 0, maximum: nil)
+
+
 
 print ("How many stitches wide is your gauge?")
 print("C - Celsius")
